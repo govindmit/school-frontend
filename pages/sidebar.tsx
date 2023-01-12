be7db27eca4f30047def4ca1ecc564552d8d792d
jaydeep <jaydeepmangoit721@gmail.com>
2023-01-12 18:49:10 +0530

    work on dashboard styles

--- pages/sidebar.tsx
+++ pages/sidebar.tsx
@@ -188,12 +188,59 @@ export default function MiniDrawer() {
                 }}
               ></ListItemIcon>
               <ListItemText
-                primary="Guardians"
+                primary="Customers"
                 sx={{ opacity: open ? 1 : 0 }}
               />
             </ListItemButton>
           </ListItem>
 
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => router.push("/admin/invoices")}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              ></ListItemIcon>
+              <ListItemText primary="Invoices" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => router.push("/admin/creditNotes")}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              ></ListItemIcon>
+              <ListItemText
+                primary="Credit Notes"
+                sx={{ opacity: open ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
           <ListItem
             disablePadding
             sx={{ display: "block" }}
@@ -214,6 +261,28 @@ export default function MiniDrawer() {
                 }}
               ></ListItemIcon>
               <ListItemText primary="Activity" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            // onClick={() => router.push("/activites/activitylist")}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              ></ListItemIcon>
+              <ListItemText primary="Log Out" sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
           </ListItem>
         </List>
