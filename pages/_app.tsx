73d504fc13976573189320574cf673107184bc18
jaydeep <jaydeepmangoit721@gmail.com>
2023-01-23 19:39:17 +0530

    work on invoice

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import "../styles/invoice.css";
 import "../styles/dashboard.css";
 import "../styles/customer.css";
 import "../styles/globals.css";
+import "../styles/addinvoice.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
